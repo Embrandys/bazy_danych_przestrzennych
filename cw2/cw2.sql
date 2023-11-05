037061ca7264acd22d3a389dcbb432e1ec19bbd6
Emilia Brandys <embrandys@student.agh.edu.pl>
2023-11-05 21:46:56 +0100

    all

--- cw2/cw2.sql
+++ cw2/cw2.sql
@@ -40,43 +40,51 @@ INSERT INTO drogi VALUES (1,ST_GeomFromText('LINESTRING (0.0 4.5, 12.0 4.5)'), '
 INSERT INTO drogi VALUES (2,ST_GeomFromText('LINESTRING (7.5 10.5, 7.5 0)'), 'RoadY' );
 
 --6
---a
-SELECT SUM(ST_Length(geometria)) 
+--a Wyznacz całkowitą długość dróg w analizowanym mieście.
+SELECT SUM(ST_Length(geometria))
 FROM drogi;
 
---b
+--b Wypisz geometrię (WKT), pole powierzchni oraz obwód poligonu reprezentującego
+--  budynek o nazwie BuildingA.
 SELECT ST_AsText(geometria) AS "WKT" , 
 ST_Area(geometria) AS "Pole powierzchni", 
 ST_Perimeter(geometria) AS "Obwod" 
-FROM budynki;
+FROM budynki
+WHERE nazwa='BuildingA';
 
---c
+--c Wypisz nazwy i pola powierzchni wszystkich poligonów w warstwie budynki. Wyniki
+--  posortuj alfabetycznie.
 SELECT nazwa, ST_Area(geometria) AS "Pole powierzchni" 
 FROM budynki 
 ORDER BY nazwa;
 
---d
+--d Wypisz nazwy i obwody 2 budynków o największej powierzchni.
 SELECT nazwa, ST_Perimeter(geometria) AS "Obwod" 
 FROM budynki 
 ORDER BY ST_Area(geometria) DESC 
 LIMIT 2;
 
---e
+--e Wyznacz najkrótszą odległość między budynkiem BuildingC a punktem G
 SELECT ST_Distance(budynki.geometria, punkty_informacyjne.geometria)
 FROM budynki, punkty_informacyjne
 WHERE budynki.nazwa='BuildingC' AND punkty_informacyjne.nazwa='G';
 
---f
+--f Wypisz pole powierzchni tej części budynku BuildingC, która znajduje się w
+--  odległości większej niż 0.5 od budynku BuildingB
 SELECT ST_Area(ST_Difference(budC.geometria, ST_Buffer(budB.geometria, 0.5))) AS "Pole powierzchni"
 FROM budynki budB, budynki budC
 WHERE budB.nazwa='BuildingB' AND budC.nazwa='BuildingC';
 
---g
+--g Wybierz te budynki, których centroid (ST_Centroid) znajduje się powyżej drogi
+--  o nazwie RoadX.
 SELECT budynki.nazwa, drogi.geometria 
 FROM budynki, drogi
 WHERE ST_Y(ST_Centroid(budynki.geometria)) > ST_Y(ST_Centroid(drogi.geometria))
 AND drogi.nazwa = 'RoadX';
 
-SELECT ST_Area(ST_SymDifference(geometria, ST_GeomFromText('POLYGON((4.0 7.0, 6.0 7.0, 6.0 8.0, 4.0 8.0, 4.0 7.0))')))
+--Oblicz pole powierzchni tych części budynku BuildingC i poligonu
+--o współrzędnych (4 7, 6 7, 6 8, 4 8, 4 7), które nie są wspólne dla tych dwóch
+--obiektów.
+SELECT ST_Area(ST_SymDifference(geometria, ST_GeomFromText('POLYGON((4.0 7.0, 6.0 7.0, 6.0 8.0, 4.0 8.0, 4.0 7.0))'))) AS "Pole"
 FROM budynki
 WHERE nazwa='BuildingC';
